13c434d64f691d17fa13a2704463ac0f9db9f133
fomly <abdulahi2700@gmail.com>
2025-04-24 12:22:52 +0200

    fix some stupid changes

--- pageT.js
+++ pageT.js
@@ -1,12 +1,14 @@
 //Person B skriver sin kode her 
 function setup(){
     createCanvas(400,400)
+  
     textSize(20)
     textAlign(CENTER, CENTER);
     text("køb billetter",width/2,height/2)
     let buttonBack = createButton('go back');
     buttonBack.position(width/2-200,height/2-200);
     buttonBack.mousePressed(goHome);
+    
 
     //Udkommenterer de filer som andre arbejder på,
     //hvis de ikke virker endnu.    
